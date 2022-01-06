3f4396623adafbc67d3dd9ae448d87a9e9d50c5c
Avan <60692794+Layouwen@users.noreply.github.com>
2022-01-06 11:21:46 +0800

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import FirstScreen from './pages/FirstScreen';
+
+function App () {
+  return (
+    <div className="App">
+      <FirstScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/FirstScreen/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const FirstScreen: FC = () => {
+  return <div>首屏</div>;
+};
+
+export default FirstScreen;
