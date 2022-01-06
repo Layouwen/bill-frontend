869c2334cad360a2d37b4031c1030d76d261514b
Avan <layouwen@gmail.com>
2022-01-06 23:22:48 +0800

    feat: add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
 import FirstScreen from './pages/FirstScreen';
 
 function App () {
+  const a = 0;
+  console.log(1);
   return (
     <div className="App">
       <FirstScreen />
