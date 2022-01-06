b0d5679b2f6b6cde9b6b581abd00ec7f4b713ae0
Avan <layouwen@gmail.com>
2022-01-07 01:22:45 +0800

    feat: add tailwind css

--- src/pages/FirstScreen/index.tsx
+++ src/pages/FirstScreen/index.tsx
@@ -1,7 +1,24 @@
 import { FC } from 'react'
+import classNames from 'classnames'
+import styles from './index.module.css'
+import logo from '../../assets/images/logo.png'
 
 const FirstScreen: FC = () => {
-  return <div>首屏</div>
+  return (
+    <div
+      className={classNames([
+        styles.bg,
+        'flex flex-col justify-center items-center h-screen',
+      ])}
+    >
+      <div
+        className={'flex flex-col justify-center items-center -translate-y-1/3'}
+      >
+        <img className={styles.logo} src={logo} alt="蓝鲸记账" />
+        <span className={styles['logo-text']}>蓝鲸记账</span>
+      </div>
+    </div>
+  )
 }
 
 export default FirstScreen
