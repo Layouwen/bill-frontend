0f4621a83fd2dd10aa44f160331f55c91d1a216f
Avan <layouwen@gmail.com>
2022-01-07 00:11:42 +0800

    feat: add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 0, // vite自动引入react不需要手动引入
     'jsx-quotes': 2, // jsx使用双引号
+    semi: [2, 'never'], // 不需要分号
   },
 }
--- src/pages/FirstScreen/index.tsx
+++ src/pages/FirstScreen/index.tsx
@@ -1,7 +1,7 @@
-import { FC } from 'react';
+import { FC } from 'react'
 
 const FirstScreen: FC = () => {
-  return <div>首屏</div>;
-};
+  return <div>首屏</div>
+}
 
-export default FirstScreen;
+export default FirstScreen
