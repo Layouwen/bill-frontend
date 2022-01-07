621e4e067695da1c073fcbbd52bfab3ddfb020a0
Avan <layouwen@gmail.com>
2022-01-07 22:33:26 +0800

    style: format code

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,26 @@
-import { ChangeEventHandler, FC } from 'react';
-import './index.scss';
+import { ChangeEventHandler, FC } from 'react'
+import './index.scss'
 
 interface BwBillInput {
-  label?: string;
-  value?: string | number;
-  placeholder?: string;
-  onChange?: ChangeEventHandler;
+  label?: string
+  value?: string | number
+  placeholder?: string
+  onChange?: ChangeEventHandler
 }
 
-const Input: FC<BwBillInput> = ({label, value, placeholder, onChange}) => {
-  return <label className="bw-bill-input-wrapper">
-    <span className="bw-bill-input-name">{label ?? '标题'}</span>
-    <input onChange={onChange} className="bw-bill-input" type="text" placeholder={placeholder} value={value} />
-  </label>;
-};
+const Input: FC<BwBillInput> = ({ label, value, placeholder, onChange }) => {
+  return (
+    <label className="bw-bill-input-wrapper">
+      <span className="bw-bill-input-name">{label ?? '标题'}</span>
+      <input
+        onChange={onChange}
+        className="bw-bill-input"
+        type="text"
+        placeholder={placeholder}
+        value={value}
+      />
+    </label>
+  )
+}
 
-export default Input;
+export default Input
