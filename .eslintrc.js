6afbb51ebf2315aecbccb46f59e1ee5f2f08af76
Avan <layouwen@gmail.com>
2022-01-07 20:06:04 +0800

    feat: add cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'cypress/globals': true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:cypress/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -16,7 +18,7 @@ module.exports = {
     ecmaVersion: 13,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'cypress'],
   rules: {
     'react/react-in-jsx-scope': 0, // vite自动引入react不需要手动引入
     'jsx-quotes': 2, // jsx使用双引号
