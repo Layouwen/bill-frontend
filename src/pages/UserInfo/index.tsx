f01c293a8aa1bd328903980299170fc950bddb78
Avan <60692794+Layouwen@users.noreply.github.com>
2022-01-18 20:24:15 +0800

    todo: add choseFile

--- src/pages/UserInfo/index.tsx
+++ src/pages/UserInfo/index.tsx
@@ -64,7 +64,6 @@ const userInfo: FC = () => {
                 styles.avatar,
                 'rounded-full overflow-hidden',
               )}
-              onClick={() => console.log('换头像')}
             >
               <img
                 className="w-full h-full object-cover"
