56e60c33c44b8c69a2ddca72534909b18d2a243a
Avan <layouwen@gmail.com>
2022-02-07 01:16:39 +0800

    feat: add share component static page

---
+++ src/components/Share/index.ts
@@ -0,0 +1,4 @@
+import { Share } from './Share';
+import './Share.scss';
+
+export default Share;
--- src/pages/TopicDetail/Main.tsx
+++ src/pages/TopicDetail/Main.tsx
@@ -1,5 +1,5 @@
 import { Topic } from '@/api';
-import { FixedPin, ImagePreview, TopicItem } from '@/components';
+import { FixedPin, ImagePreview, TopicItem, Share } from '@/components';
 import ReplyArea from '@/pages/TopicDetail/ReplyArea';
 import { FC, useState } from 'react';
 
@@ -12,10 +12,55 @@ type MainProps = {
 const Main: FC<MainProps> = ({ topic, comments, onLike }) => {
   const [imgVisible, setImgVisible] = useState(false);
   const [imgSrc, setImgSrc] = useState('');
+  const [shareVisible, setShareVisible] = useState(false);
+
+  const shares = [
+    {
+      id: 1,
+      name: '微信',
+      icon: 'wechat',
+      color: '#55BA38',
+      onClick: () => {
+        console.log('share.wechat(topic)');
+      },
+    },
+    {
+      id: 2,
+      name: '微信朋友圈',
+      icon: 'wechat-friends',
+      color: '#55BA3A',
+      onClick: () => {
+        console.log('share.wechat-friends(topic)');
+      },
+    },
+    {
+      id: 3,
+      name: 'QQ',
+      icon: 'qq',
+      color: '#4EAAF7',
+      onClick: () => {
+        console.log('share.qq(topic)');
+      },
+    },
+    {
+      id: 4,
+      name: 'QQ空间',
+      icon: 'qq-zone',
+      color: '#F3B140',
+      onClick: () => {
+        console.log('share.qq-zone(topic)');
+      },
+    },
+  ];
 
   return (
     <div className="flex-grow overflow-auto">
-      <FixedPin>分享</FixedPin>
+      <Share
+        shares={shares}
+        visible={shareVisible}
+        onClose={() => setShareVisible(false)}
+      />
+      <FixedPin onClick={() => setShareVisible(true)}>操作</FixedPin>
       <ImagePreview
         visible={imgVisible}
         image={imgSrc}
