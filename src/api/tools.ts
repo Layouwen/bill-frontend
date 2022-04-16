5ff7479127aa71e3e5e0f798f164b55797d3573a
Avan <layouwen@gmail.com>
2022-04-16 11:59:56 +0800

    feat: add sendEmail function

--- src/api/tools.ts
+++ src/api/tools.ts
@@ -3,3 +3,10 @@ import { request } from '@/utils';
 export const getCaptchaApi = () => {
   return request.get<unknown, any>('/tools/captcha');
 };
+
+export const getEmailCaptchaAPi = (email: string) => {
+  return request.get<unknown, any>('/tools/email', {
+    params: { email },
+    loading: true,
+  });
+};
